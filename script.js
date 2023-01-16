b4e1416d12c116d7b8ba1a7bdf599f8c9b418483
RobotChiken57 <99732056+RobotChiken57@users.noreply.github.com>
2023-01-16 13:36:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// № 1
+// let getInput = document.querySelectorAll('input');
+        //let getP = document.querySelector('#test');
+        //for (let i = 0; i < getInput.length; i++) {
+            //getInput[i].addEventListener('blur', func);(потеря фокуса)
+        //}
+        //function func() {
+            //getP.innerHTML = getP.innerHTML + this.value; ( вывод 'value' )
+        //}
+        //
